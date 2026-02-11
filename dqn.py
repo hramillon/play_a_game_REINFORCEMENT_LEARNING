d8cedb6baf96224858c897713de9321e99caf0ae
Hugo Ramillon <hugo.ramillon@etu.u-bordeaux.fr>
2026-02-11 17:48:40 +0100

    add images for flappy

--- dqn.py
+++ dqn.py
@@ -3,14 +3,16 @@ from torch import nn
 import torch.nn.functional as F
 
 class DQN(nn.Module):
-    def __init__(self, state_dim, action_dim, hidden_dim=256):
+    def __init__(self, state_dim, action_dim, hidden_dim=512):
         super(DQN, self).__init__()
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, action_dim)
-
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, action_dim)
+        
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        return self.fc2(x)
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
 
 if __name__ == '__main__':
     state_dim = 12
