fbf7da6f061ce1c2273ed0f83afbb57f1fad9964
Hugo Ramillon <hugo.ramillon@etu.u-bordeaux.fr>
2026-02-13 13:33:24 +0100

    flappy result

--- dqn.py
+++ dqn.py
@@ -3,39 +3,34 @@ from torch import nn
 import torch.nn.functional as F
 
 class DQN(nn.Module):
-
-    def __init__(self, state_dim, action_dim, hidden_dim=512, enable_dueling_dqn=True):
+    def __init__(self, state_dim, action_dim, hidden_dim=256, enable_dueling_dqn=True):
         super(DQN, self).__init__()
-
-        self.enable_dueling_dqn=enable_dueling_dqn
-
+        self.enable_dueling_dqn = enable_dueling_dqn
+        
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-
+        
         if self.enable_dueling_dqn:
-            self.fc_value = nn.Linear(hidden_dim, 256)
-            self.value = nn.Linear(256, 1)
-
-            self.fc_advantages = nn.Linear(hidden_dim, 256)
-            self.advantages = nn.Linear(256, action_dim)
-
+            self.fc_value = nn.Linear(hidden_dim, 128)
+            self.value = nn.Linear(128, 1)
+            self.fc_advantages = nn.Linear(hidden_dim, 128)
+            self.advantages = nn.Linear(128, action_dim)
         else:
-            self.output = nn.Linear(hidden_dim, action_dim)
-
+            self.fc2 = nn.Linear(hidden_dim, 128)
+            self.output = nn.Linear(128, action_dim)
+    
     def forward(self, x):
         x = F.relu(self.fc1(x))
-
+        
         if self.enable_dueling_dqn:
             v = F.relu(self.fc_value(x))
             V = self.value(v)
-
             a = F.relu(self.fc_advantages(x))
             A = self.advantages(a)
-
-            Q = V + A - torch.mean(A, dim=1, keepdim=True)
-
+            Q = V + (A - A.mean(dim=1, keepdim=True))
         else:
+            x = F.relu(self.fc2(x))
             Q = self.output(x)
-
+        
         return Q
 
 
